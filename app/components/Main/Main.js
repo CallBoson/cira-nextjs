d077e2627ae4f80c4f1708a41c9a5e91e3d092a5
丢那妈 <9441896+bosons@user.noreply.gitee.com>
2024-07-12 15:33:45 +0800

    1

--- app/components/Main/Main.js
+++ app/components/Main/Main.js
@@ -1,7 +1,10 @@
 "use client";
 
-import { Button } from "@nextui-org/react";
 import { Modal, useDisclosure } from "@nextui-org/modal";
+import { motion, AnimatePresence } from "framer-motion";
+import { MagicCard } from "./MagicCard";
+import { Button } from "@nextui-org/react";
+import { BsStars } from "react-icons/bs";
 
 export default function Main() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
@@ -12,24 +15,55 @@ export default function Main() {
 
   return (
     <>
-      <Button
-        color="primary"
-        onPress={onOpen}
-        className="fixed bottom-10 left-1/2 -translate-x-1/2"
-      >
-        Start free trial
-      </Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <div
-          className="absolute left-0 top-0 w-full h-full bg-[rgba(0,0,0,.2)] flex justify-center items-center"
-          onClick={handleClickMask}
+      <div className="fixed bottom-14 left-1/2 -translate-x-1/2">
+        <Button
+          onPress={onOpen}
+          startContent={<BsStars className="text-2xl" />}
+          size="lg"
+          className="bg-gradient-to-tr from-pink-500 to-yellow-500 shadow-lg rounded-full"
         >
+          Try me
+        </Button>
+      </div>
+
+      <AnimatePresence>
+        <Modal isOpen={isOpen}>
           <div
-            className="w-[500px] h-[400px] bg-[rgba(255,255,255,0.3)] backdrop-blur rounded-xl"
-            onClick={(e) => e.stopPropagation()}
-          ></div>
-        </div>
-      </Modal>
+            className="absolute left-0 top-0 w-full h-full bg-[rgba(0,0,0,.2)] flex justify-center items-center"
+            onClick={handleClickMask}
+          >
+            <motion.div
+              className=" bg-[rgb(13,13,13)] backdrop-blur-xl rounded-xl"
+              onClick={(e) => e.stopPropagation()}
+              initial={{ scale: 0.8, opacity: 0, y: 100 }}
+              animate={{
+                scale: 1,
+                opacity: 1,
+                y: 0,
+                transition: { type: "spring", stiffness: 100 },
+              }}
+              exit={{
+                scale: 0.5,
+                opacity: 0,
+                y: 50,
+                transition: { duration: 0.3 },
+              }}
+            >
+              <MagicCard
+                gradientColor="#262626"
+                className="border border-[rgba(255,255,255,.1)]"
+              >
+                <div className="relative w-[800px] h-[600px]">
+                  <img
+                    src="https://web.clay.earth/assets/gradient_bg-6c96905f.png"
+                    className="absolute inset-0 left-0 top-1/2 -translate-y-1/2 w-full h-full blur-md saturate-150 opacity-[.65]"
+                  />
+                </div>
+              </MagicCard>
+            </motion.div>
+          </div>
+        </Modal>
+      </AnimatePresence>
     </>
   );
 }
--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import ShimmerButton from "../ShimmerButton";
 import { FaGithub } from "react-icons/fa";
